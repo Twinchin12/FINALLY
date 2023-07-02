2917f08e3b006ae2db325266f3ffb54881fa7ce5
kamikaonashi <thorschi93@gmx.at>
2023-07-02 11:53:22 +0200

    sm8350-common: enable lineage health charging control

--- common.mk
+++ common.mk
@@ -200,6 +200,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default    
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
